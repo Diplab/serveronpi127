b002760f8700532e6523589a70892a5bec2bfc3d
yingjhu <yingjhu@hotmail.com>
2015-11-13 15:18:14 +0800

    Update Function

--- src/main/java/com/diplab/webservice/Device127.java
+++ src/main/java/com/diplab/webservice/Device127.java
@@ -9,14 +9,11 @@ import javax.xml.ws.Endpoint;
 import com.diplab.service.CO2Service;
 import com.diplab.service.COService;
 import com.diplab.service.SmokeService;
-import com.diplab.service.SwitchService;
 import com.diplab.service.TemperatureService;
 import com.diplab.serviceImp.CO2ServiceImpl;
 import com.diplab.serviceImp.COServiceImpl;
 import com.diplab.serviceImp.SmokeServiceImpl;
-import com.diplab.serviceImp.SwitchServiceImpl;
 import com.diplab.serviceImp.TemperatureServiceImpl;
-import com.pi4j.io.gpio.PinState;
 
 @WebService
 @SOAPBinding(style = Style.RPC)
@@ -25,28 +22,7 @@ public class Device127 {
 	CO2Service co2Service = new CO2ServiceImpl();
 	COService coService = new COServiceImpl();
 	SmokeService smokeService = new SmokeServiceImpl();
-	SwitchService SwitchService = new SwitchServiceImpl();
 	TemperatureService temperatureService = new TemperatureServiceImpl();
-
-	@WebMethod
-	public void off() {
-		SwitchService.off();
-	}
-
-	@WebMethod
-	public void on() {
-		SwitchService.on();
-	}
-
-	@WebMethod
-	public void toggle() {
-		SwitchService.toggle();
-	}
-
-	@WebMethod
-	public PinState getState() {
-		return SwitchService.getState();
-	}
 
 	@WebMethod
 	public double CO2ppm() {
@@ -72,7 +48,7 @@ public class Device127 {
 		Endpoint.publish("http://0.0.0.0:9005/webservice/Device127",
 				new Device127());
 
-		System.out.println("open webservice127 Escape");
+		System.out.println("open webservice127");
 
 	}
 
